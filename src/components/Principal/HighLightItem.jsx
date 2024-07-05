dc897312b868d4740a1b2cefbcce53925e2acf6d
Enzo <enzolucasmarchi@gmail.com>
2024-07-05 07:33:01 -0300

    sizing details

--- src/components/Principal/HighLightItem.jsx
+++ src/components/Principal/HighLightItem.jsx
@@ -1,6 +1,6 @@
 export const HighlightItem = ({children, title, datavalue, metric}) => {
     return(
-        <div className="bg-softblue w-full h-28 flex flex-col gap-2 items-center py-2">
+        <div className="bg-softblue w-full h-28 flex flex-col gap-1 justify-center items-center">
         <p className="text-xs">{title}</p>
         <p className="text-4xl text-bold">{datavalue}<span className="text-xs">{metric}</span></p>
         {children && <div>
